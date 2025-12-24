af9f0529406f9ce404da6fbb4f6b96c65e4aece2
sameer4712 <sameerptmc@gmail.com>
2025-12-24 15:40:00 +0530

    1

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import AboutSection from '@/components/aboutPage'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+        <AboutSection />
+    </div>
+  )
+}
